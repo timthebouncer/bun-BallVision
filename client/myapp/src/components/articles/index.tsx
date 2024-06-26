7b3b27855319a96372602e228bd360fb5f23a4f4
timthebouncer <he01367637us@gmail.com>
2024-06-26 23:37:05 +0800

    feat: react-tweet

--- client/myapp/src/components/articles/index.tsx
+++ client/myapp/src/components/articles/index.tsx
@@ -1,14 +1,24 @@
+import {useEffect} from "react";
+import * as React from "react";
+import { TweetSkeleton, EmbeddedTweet, TweetNotFound } from 'react-tweet'
+import { fetchTweet, Tweet } from 'react-tweet'
+import './base.css'
 
 const SingleArticle=()=>{
-    return <div>
-        <blockquote className="twitter-tweet">
-            <p lang="en" dir="ltr">
-                Most people don&#39;t tag their precise location in Tweets, so we&#39;re removing this ability to simplify your Tweeting experience.
-                You&#39;ll still be able to tag your precise location in Tweets through our updated camera. It&#39;s helpful when sharing on-the-ground moments.
-            </p>&mdash; Support (@Support)
-            <a href="https://twitter.com/Support/status/1141039841993355264?ref_src=twsrc%5Etfw">June 18, 2019</a>
-        </blockquote>
-        <blockquote className="twitter-tweet"><p lang="en" dir="ltr">At dawn from the gateway to Mars, the launch of Starship’s second flight test <a href="https://t.co/ffKnsVKwG4">pic.twitter.com/ffKnsVKwG4</a></p>&mdash; SpaceX (@SpaceX) <a href="https://twitter.com/SpaceX/status/1732824684683784516?ref_src=twsrc%5Etfw">December 7, 2023</a></blockquote>
+
+    useEffect(()=>{
+
+    },[])
+
+
+
+        return <div>
+            <div className='test'>
+                <Tweet id="1805687110566826297" />
+            </div>
+            <div className='test'>
+                <Tweet id="1805798876139634709" />
+            </div>
     </div>
 }
 
