a118e263f9daeb0d4813e388adf90785cc71a19d
tim.wty <tim.wty@link8tech.tw>
2024-06-26 22:14:33 +0800

    fake data

---
+++ client/myapp/data/navList.js
@@ -0,0 +1,27 @@
+export const navList=[
+  { id: 'nav1',
+    name: 'NBA',
+    subList: [
+      { id: 'sub1', name: '最新資訊'},
+      { id: 'sub2', name: '新聞報導' },
+    ],
+    url: ''
+  },
+  { id: 'nav2',
+    name: '台籃',
+    subList: [
+      { id: 'sub1', name: '最新資訊'},
+      { id: 'sub2', name: '新聞報導'},
+    ],
+    url: ''
+  },
+  { id: 'nav3',
+    name: '文章',
+    subList: [],
+    url: '/articles'
+  },
+  { id: 'nav4',
+    name: '購物',
+    url: ''
+  },
+]
