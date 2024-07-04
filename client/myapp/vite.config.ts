26650d428fd4d7cec86f65e0658518d348f62947
tim.wty <tim.wty@link8tech.tw>
2024-07-04 13:24:26 +0800

    feat: target

--- client/myapp/vite.config.ts
+++ client/myapp/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://192.168.168.143',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         configure: (proxy, options) => {
