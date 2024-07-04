72122da72e8fc2c65161eea04375b3e9c2a9337f
timthebouncer <he01367637us@gmail.com>
2024-07-04 22:33:14 +0800

    feat: 域名

--- client/myapp/vite.config.ts
+++ client/myapp/vite.config.ts
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/bun-BallVision",
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://192.168.1.108',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //       configure: (proxy, options) => {
-  //       }
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.1.108',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+        }
+      },
+    },
+  },
 })
