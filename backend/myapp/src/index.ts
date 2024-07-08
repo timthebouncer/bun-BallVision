933c41511d40eda67537c2c09f0da3ce3e381d84
timthebouncer <he01367637us@gmail.com>
2024-07-08 22:48:20 +0800

    refactor

--- backend/myapp/src/index.ts
+++ backend/myapp/src/index.ts
@@ -1,52 +1,22 @@
-import Elysia, {t} from "elysia";
+import {t} from "elysia";
 import {cors} from '@elysiajs/cors'
-import { swagger } from '@elysiajs/swagger';
-// import { env } from './env';
-import {z} from "zod";
-
-// import {app, db} from "./constants";
-// import {migrate} from "drizzle-orm/bun-sqlite/migrator";
-// import './controller/video.controller'
-// import './controller/article.controller'
-
-const envVariables = z.object({
-    NODE_ENV: z
-        .enum(['development', 'production', 'test'])
-        .default('development'),
-    PORT: z.coerce.number().default(3000),
-    RUNTIME: z.enum(['bun', 'edge']).default('bun'),
-});
-
-const env = envVariables.parse(process.env);
-
-const createElysia = (
-    config?: ConstructorParameters<typeof Elysia>[0]
-) => new Elysia({ ...config, aot: env.RUNTIME === 'bun' });
-
-const config = { runtime: 'edge' };
 
 
-;(async ()=>{
+import {app, db} from "./constants";
+import {migrate} from "drizzle-orm/bun-sqlite/migrator";
+import './controller/video.controller'
+import './controller/article.controller'
 
-    const app = createElysia()
 
-  // await migrate(db, { migrationsFolder: "./drizzle" });
+    ;(async ()=>{
 
-  app
-      .use(swagger())
-      .use(cors())
-      .get('/www',()=>{
-        return 'wwwwww'
-      })
+    await migrate(db, { migrationsFolder: "../drizzle" });
 
-    app.listen({ port: env.PORT }, ({ hostname, port }) => {
-        const url = env.NODE_ENV === 'production' ? 'https' : 'http';
+    app
+        .use(cors())
+        .listen(3000)
 
-        console.log(`🦊 Elysia is running at ${url}://${hostname}:${port}`);
-    });
-
-    // console.log(
-    //     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-    // );
+    console.log(
+        `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    );
 })()
-
