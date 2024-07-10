84e87538097a9ac718ae020295ca726b7efbc548
tim.wty <tim.wty@link8tech.tw>
2024-07-10 18:12:05 +0800

    feat: video turso

--- backend/myapp/src/dao/video.dao.ts
+++ backend/myapp/src/dao/video.dao.ts
@@ -1,6 +1,8 @@
 import {videos} from "../db/schema";
 import {asc, count, desc, eq, like} from "drizzle-orm";
 import {db} from "../constants";
+import {getBallVisionDb} from "../db";
+import {config} from "../config";
 
 //對應表的所有sql操作
 
@@ -13,18 +15,23 @@ export class VideoDao {
 
         const response: any = {};
 
+        const { ballVisionDb } = getBallVisionDb({
+            dbName: config.env.DATABASE_URL,
+            authToken: config.env.DATABASE_AUTH_TOKEN!,
+        });
+
         if(keyword){
-            response.list = await db.select().from(videos).where(like(videos.title, `%${keyword}%`)).orderBy(desc(videos.createTime))
+            response.list = await ballVisionDb.select().from(videos).where(like(videos.title, `%${keyword}%`)).orderBy(desc(videos.createTime))
             const totalElement =
-                await db.select({ count: count() })
+                await ballVisionDb.select({ count: count() })
                     .from(videos)
                     .where(like(videos.title, `%${keyword}%`))
                     .orderBy(desc(videos.createTime))
             response.totalElement = totalElement[0].count
 
         } else {
-            response.list = await db.select().from(videos).limit(pageSize).offset((pageNumber - 1) * pageSize).orderBy(desc(videos.createTime))
-            const totalElement = await db.select({ count: count() }).from(videos)
+            response.list = await ballVisionDb.select().from(videos).limit(pageSize).offset((pageNumber - 1) * pageSize).orderBy(desc(videos.createTime))
+            const totalElement = await ballVisionDb.select({ count: count() }).from(videos)
             response.totalElement = totalElement[0].count
         }
 
@@ -33,12 +40,20 @@ export class VideoDao {
 
     // Add a todo
     async addVideo(params: UploadVideoText) {
-        await db.insert(videos).values({ ...params })
+        const { ballVisionDb } = getBallVisionDb({
+            dbName: config.env.DATABASE_URL,
+            authToken: config.env.DATABASE_AUTH_TOKEN!,
+        });
+        await ballVisionDb.insert(videos).values({ ...params })
     }
 
     // Update a todo
     async updateVideo({id, videoUrl}: UpdateVideo) {
-        return db.update(videos)
+        const { ballVisionDb } = getBallVisionDb({
+            dbName: config.env.DATABASE_URL,
+            authToken: config.env.DATABASE_AUTH_TOKEN!,
+        });
+        return ballVisionDb.update(videos)
             .set({videoUrl})
             .where(eq(videos.id, id))
     }
